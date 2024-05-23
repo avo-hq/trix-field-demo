06d0cd1728f8e709d3c45c4b83d76db946d6840f
Adrian Marin <adrian@adrianthedev.com>
2024-05-23 10:58:19 +0300

    redirect root to avo

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,6 @@
 Rails.application.routes.draw do
+  root to: redirect("/avo")
+
   mount Avo::Engine, at: Avo.configuration.root_path
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
