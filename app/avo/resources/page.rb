6c5f6679e7b6ce31960a5189b174b136a8c8187a
Adrian Marin <adrian@adrianthedev.com>
2024-05-23 10:52:21 +0300

    initial commit

---
+++ app/avo/resources/page.rb
@@ -0,0 +1,12 @@
+class Avo::Resources::Page < Avo::BaseResource
+  self.includes = []
+  # self.search = {
+  #   query: -> { query.ransack(id_eq: params[:q], m: "or").result(distinct: false) }
+  # }
+
+  def fields
+    field :id, as: :id
+    field :title, as: :text
+    field :content, as: :trix, attachment_key: :files
+  end
+end
---
+++ app/models/page.rb
@@ -0,0 +1,3 @@
+class Page < ApplicationRecord
+  has_many_attached :files
+end
